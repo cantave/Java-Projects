221ace715e7534dbb9f2712fcc73ffd0485f8d73
cantave <32780807+cantave@users.noreply.github.com>
2024-04-13 19:35:17 -0500

    completed my jeezus project! haha!

--- API Projects/KanyeRest/kanyeRest.js
+++ API Projects/KanyeRest/kanyeRest.js
@@ -1,7 +1,17 @@
-const yehURL = 'https://api.kanye.rest/';
+const yehURL = "https://api.kanye.rest/";
 
-document.addEventListener('DOMContentLoaded', () => {
-
-
-    
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const message = document.getElementById("message");
+  const button = document.getElementById("randomButton");
+  button.addEventListener("click", () => {
+    fetch(yehURL)
+      .then((httpResponse) => httpResponse.json())
+      .then((data) => {
+        message.innerText = data.quote;
+      })
+      .catch((error) => {
+        console.error('Error', error);
+        message.innerText = 'Failed to feth Yeezus Quote';
+      });
+  });
+});
